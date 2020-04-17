ef2a4d10ae0e070772d2cad25eac7d494fd42795
upendra431 <uppu431@gmail.com>
2020-04-18 00:08:25 +0530

    Birthday Chocolate

---
+++ Birthday Chocolate/src/com/upendra/BirthdayChocolate.java
@@ -0,0 +1,52 @@
+/**
+ * 
+ */
+package com.upendra;
+
+import static java.util.stream.Collectors.toList;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.List;
+import java.util.stream.Stream;
+public class BirthdayChocolate {
+
+	
+	// Complete the birthday function below.
+    static int birthday(List<Integer> s, int d, int m) {
+
+
+    }
+	/**
+	 * @param args
+	 */
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<Integer> s = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+            .map(Integer::parseInt)
+            .collect(toList());
+
+        String[] dm = bufferedReader.readLine().replaceAll("\\s+$", "").split(" ");
+
+        int d = Integer.parseInt(dm[0]);
+
+        int m = Integer.parseInt(dm[1]);
+
+        int result = birthday(s, d, m);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+
+}
